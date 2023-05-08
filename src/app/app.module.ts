392d5454056ba9b54e3abfc8ee25232bdee5ab0c
gokuljsparkout <gokul@sparkouttech.com>
2023-05-08 11:30:18 +0530

    String Interpolations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
 @NgModule({
-  declarations: [AppComponent, SuccessAlertComponent, WarningAlertComponent,],
+  declarations: [AppComponent, ServersComponent, ServerComponent,],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
