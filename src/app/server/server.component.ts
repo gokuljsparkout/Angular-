848351df590f9edd1509c15d73a0181a325527a4
gokuljsparkout <gokul@sparkouttech.com>
2023-05-08 16:42:12 +0530

    ngFor

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,8 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent {
   serverId: number = 136;
-  serverStatus: string = 'success';
+  serverStatus: string = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No Server was Created';
   serverName = 'Test Server';
+  servers = ['Test Server 1', 'Test Server 2'];
   serverCreated = false;
   constructor() {
     setTimeout(() => (this.allowNewServer = true), 2000);
@@ -18,11 +19,11 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server was created....Name is ' + this.serverName;
   }
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
- 
 }
