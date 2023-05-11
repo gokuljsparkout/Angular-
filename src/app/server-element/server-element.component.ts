5b00a99696dac94a69fd3ce535222523c09b8ba4
gokuljsparkout <gokul@sparkouttech.com>
2023-05-11 12:45:04 +0530

    LifeCycle Hooks - Init , Changes

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: { type: string, name: string ,content: string };
-  //With this element property is declared properly
-  //Now any parent component hosting this component
-  //Is able to bind to this element
-  constructor() {}
+export class ServerElementComponent implements OnInit, OnChanges {
+  @Input('srvElement') element: { type: string; name: string; content: string };
 
-  ngOnInit() {}
+  constructor() {
+    console.log('constructor called');
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ng-OnChanges called');
+    console.log(changes);
+  }
+  ngOnInit() {
+    console.log('ngInit called');
+  }
 }
