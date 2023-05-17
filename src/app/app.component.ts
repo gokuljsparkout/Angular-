f81c1bfdd28ddce2bcc46961e7460fb72a92b9fa
gokuljsparkout <gokul@sparkouttech.com>
2023-05-17 15:41:21 +0530

    Structural Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [];
-
-  onServerAdded(serverData: { serverName: string; serverContent: string }) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent,
-    });
-  }
-
-  onChangeFirst() {
-    this.serverElements[0].name = 'Changes';
-  }
-  onBlueprintAdded(blueprintData: {
-    blueprintName: string;
-    blueprintContent: string;
-  }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.blueprintName,
-      content: blueprintData.blueprintContent,
-    });
-  }
+  
+  // serverElements = [];
+  // onServerAdded(serverData: { serverName: string; serverContent: string }) {
+  //   this.serverElements.push({
+  //     type: 'server',
+  //     name: serverData.serverName,
+  //     content: serverData.serverContent,
+  //   });
+  // }
+  // onChangeFirst() {
+  //   this.serverElements[0].name = 'Changes';
+  // }
+  // onBlueprintAdded(blueprintData: {
+  //   blueprintName: string;
+  //   blueprintContent: string;
+  // }) {
+  //   this.serverElements.push({
+  //     type: 'blueprint',
+  //     name: blueprintData.blueprintName,
+  //     content: blueprintData.blueprintContent,
+  //   });
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
+import { OddComponent } from './directives/odd/odd.component';
+// import { CockpitComponent } from './cockpit/cockpit.component';
+// import { ServerElementComponent } from './server-element/server-element.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CockpitComponent,
-    ServerElementComponent,
+    OddComponent,
+    // CockpitComponent,
+    // ServerElementComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
