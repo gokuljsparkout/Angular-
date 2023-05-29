88c68c8eda8895fbba5508b8d5908cfb20bcecff
gokuljsparkout <gokul@sparkouttech.com>
2023-05-29 17:29:16 +0530

    Fetching Router Parameters

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,7 +9,12 @@ import { Component } from '@angular/core';
 export class UserComponent {
   user: { id: number; name: string };
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.user ={
+      id : this.route.snapshot.params['id'] ,
+      name : this.route.snapshot.params['name'] ,
+    }
+  }
 }
