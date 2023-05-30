0854feaae65f876e67812257d3a391986b458fb7
gokuljsparkout <gokul@sparkouttech.com>
2023-05-30 16:03:40 +0530

    Protetcting Routes with canActivate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,22 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ServersComponent, UsersComponent, UserComponent,EditServerComponent, ServerComponent, PageNotFoundComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    UsersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent,
+    PageNotFoundComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
