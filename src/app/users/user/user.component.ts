ae92f955288b030fd8ce74735449559a17a1a55c
gokuljsparkout <gokul@sparkouttech.com>
2023-05-30 10:55:03 +0530

    Route Observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
-
+  paramsSubscription: Subscription;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
@@ -17,9 +18,13 @@ export class UserComponent {
       name: this.route.snapshot.params['name'],
     };
 
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.user.id = params['id'];
       this.user.name = params['name'];
     });
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 }
