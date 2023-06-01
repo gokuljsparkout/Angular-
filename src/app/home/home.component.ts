a1cd7e22778c201d96c0ba5ede4f35c512e1045f
gokuljsparkout <gokul@sparkouttech.com>
2023-06-01 12:27:49 +0530

    Implementing Completion and Handling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Subscription, interval } from 'rxjs';
 import { Observable } from 'rxjs-compat';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -18,20 +19,32 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count++);
+        if (count === 2) {
+          observer.complete();
+        }
         if (count > 3) {
           observer.error(new Error('Count is Greater than 3'));
         }
       }, 1000);
     });
-    this.intervalSubscription = customIntervalObservable.subscribe(
-      (count) => {
-        console.log(count);
-      },
-      (error) => {
-        console.log(error);
-        alert(error.message);
-      }
-    );
+    this.intervalSubscription = customIntervalObservable
+      .pipe(
+        map((data: number) => {
+          return 'Round' + (data + 1);
+        })
+      )
+      .subscribe(
+        (count) => {
+          console.log(count);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log('Complete');
+        }
+      );
   }
   ngOnDestroy() {
     this.intervalSubscription.unsubscribe();
