e31f29f49a8d2e27870a61538a40199b6de19a10
gokuljsparkout <gokul@sparkouttech.com>
2023-06-02 12:55:07 +0530

    Submitting and using the form object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +8,10 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value.email);
   }
   ngOnInit() {}
   constructor() {}
