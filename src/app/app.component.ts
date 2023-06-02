5f53e6d7a5295ab04138c513ff8fa95ea6e7c539
gokuljsparkout <gokul@sparkouttech.com>
2023-06-02 17:49:46 +0530

    Setting Form Value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,22 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   defaultQuestion: string = 'pet';
+  defaultGender: string = 'male';
   answer: string;
+  genders: string[] = ['male', 'female'];
   @ViewChild('form') form: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.form.value.userdata.username = suggestedName;
+    //This method does not trigger ChangeDetection
+    console.log(this.form.value);
+    this.form.setValue({
+      ...this.form.value,
+      userdata: {
+        ...this.form.value.userdata,
+        username: suggestedName,
+      },
+    });
   }
 
   show() {}
