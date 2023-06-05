11a285e8129c2f91f52122a0dff3de650590c7e1
gokuljsparkout <gokul@sparkouttech.com>
2023-06-05 18:24:45 +0530

    Creating Custom Variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,24 +14,38 @@ import {
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames= ['Anna', 'Chris'];
 
   onSubmit() {
     console.log(this.signupForm);
   }
 
   onAddHobby() {
-    const control = new FormControl(null, Validators.required);
+    const control = new FormControl(null, [Validators.required]);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
 
   getControls() {
     return (this.signupForm.get('hobbies') as FormArray).controls;
   }
-  
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    console.log(this.forbiddenUsernames.indexOf(control.value));
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+    
+      return {
+        nameIsForbidden: true,
+      };
+    }
+    return null;
+  }
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl('Gokul', Validators.required),
+        username: new FormControl('Gokul', [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
         email: new FormControl(null, [Validators.email, Validators.required]),
       }),
       gender: new FormControl('male'),
