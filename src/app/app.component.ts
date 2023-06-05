3b11a08b5b6c8dcb15e1ded4f858934abc425e8a
gokuljsparkout <gokul@sparkouttech.com>
2023-06-05 12:36:22 +0530

    Reactive Forms Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  @ViewChild('form') form: NgForm;
-
-  defaultSubscription = 'Advanced';
-  subscriptions = ['Basic', 'Advanced', 'Pro'];
-  submitted: boolean = false;
-  user = {
-    email: '',
-    subscription: '',
-    password: '',
-  };
-  pwd = '';
-
-  check() {
-    console.log('change');
-    if (this.pwd.length < 8) {
-      // Update the validation status of the password field to 'INVALID'
-      this.form.controls['password'].setErrors({ invalid: true });
-    } else {
-      // Reset the validation status of the password field
-      this.form.controls['password'].setErrors(null);
-    }
-  }
-
-  onSubmit() {
-    if (this.form.valid === false) {
-      alert('Form is invalid');
-    } else {
-      console.log(this.form);
-      this.user.email = this.form.value.email;
-      this.user.subscription = this.form.value.subscription;
-      this.user.password = this.form.value.password;
-      this.submitted = true;
-      // this.form.reset();
-    }
-  }
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
   ngOnInit() {}
 
   constructor() {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
