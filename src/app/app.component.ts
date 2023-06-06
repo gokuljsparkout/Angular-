ff688e46cc3dfb7c49ab37ab9ad7557e3eceb15b
gokuljsparkout <gokul@sparkouttech.com>
2023-06-06 13:08:32 +0530

    Reactive Form - Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-root',
@@ -8,57 +8,47 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  genders = ['male', 'female'];
-  signupForm: FormGroup;
-  forbiddenUsernames = ['Anna', 'Chris'];
+  projectForm: FormGroup;
 
-  onSubmit() {
-    console.log(this.signupForm.value);
-  }
+  projectStatus = ['Stable', 'Critical', 'Finished'];
 
-  onAddHobby() {
-    const control = new FormControl(null, Validators.required);
-    (this.signupForm.get('hobbies') as FormArray).push(control);
-  }
-
-  getControls() {
-    return (this.signupForm.get('hobbies') as FormArray).controls;
-  }
-
-  forbiddenNames(control: FormControl): { [s: string]: boolean } {
-    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
-      return { nameIsForbidden: true };
+  forbiddenProjectName(control: FormControl): { [s: string]: boolean } {
+    if (control.value === 'Test') {
+      return {
+        forbiddenName: true,
+      };
     }
     return null;
   }
-
-  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
-    return new Promise<any>((resolve) => {
+  forbiddenEmail(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise((resolve) => {
       setTimeout(() => {
-        if (control.value === 'sceren@gmail.com') {
-          resolve({ emailIsForbidden: true });
-        } else {
-          resolve(null);
+        if (control.value === 'g@g.com') {
+          resolve({
+            forbiddenEmail: true,
+          });
         }
+        resolve(null);
       }, 1500);
     });
   }
 
   ngOnInit() {
-    this.signupForm = new FormGroup({
-      userData: new FormGroup({
-        username: new FormControl('Gokul', [
-          Validators.required,
-          this.forbiddenNames.bind(this),
-        ]),
-        email: new FormControl(
-          null,
-          [Validators.email, Validators.required],
-          [this.forbiddenEmails.bind(this)]
-        ),
-      }),
-      gender: new FormControl('male'),
-      hobbies: new FormArray([]),
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(null, [
+        Validators.required,
+        this.forbiddenProjectName.bind(this),
+      ]),
+      email: new FormControl(
+        null,
+        [Validators.required, Validators.email],
+        [this.forbiddenEmail.bind(this)]
+      ),
+      status: new FormControl('Critical'),
     });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value.projectName);
   }
 }
