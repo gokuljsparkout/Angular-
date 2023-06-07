9c44a919e9b812ac8c119faf11f9cd91b28e9013
gokuljsparkout <gokul@sparkouttech.com>
2023-06-07 18:27:05 +0530

    Parameterizing Custom Pipes

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -3,9 +3,9 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'shorten',
 })
 export class ShortenPipe implements PipeTransform {
-  transform(value: any) {
+  transform(value: any,limit :number) {
     if (value.length > 10) {
-      return value.substr(0, 10) + '...';
+      return value.substr(0, limit) + '...';
     } else {
       return value;
     }
