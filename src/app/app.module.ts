6f4e494be5e8552d03ee35e341edc4094467d426
gokuljsparkout <gokul@sparkouttech.com>
2023-06-07 18:25:01 +0530

    Custom Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ShortenPipe } from './shorten.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ShortenPipe],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
