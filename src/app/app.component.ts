71fee1a67721fbd913b325679dba579f2dd3be94
gokuljsparkout <gokul@sparkouttech.com>
2023-06-08 10:01:54 +0530

    Filter Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs-compat';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  filteredStatus = '';
   servers = [
     {
       instanceType: 'medium',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  declarations: [AppComponent, ShortenPipe, FilterPipe],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
