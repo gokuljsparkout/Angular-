7a70bb91dece7c93a258a39484ff3e9a88fc8ba8
gokuljsparkout <gokul@sparkouttech.com>
2023-06-20 11:32:11 +0530

    Response Interceptor

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,10 +1,12 @@
 import {
   HttpEvent,
+  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptor implements HttpInterceptor {
   intercept(
@@ -14,6 +16,13 @@ export class AuthInterceptor implements HttpInterceptor {
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz'),
     });
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(
+      tap((event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Response Arrived . body data : ');
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
