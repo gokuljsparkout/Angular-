923952172a69df52a00c7ab94a633d9ab5aa03ef
gokuljsparkout <gokul@sparkouttech.com>
2023-06-20 12:10:37 +0530

    interval , take rxjs operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { of } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { interval, of } from 'rxjs';
+import { take } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,10 @@ export class AppComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    of<number>(1, 2, 3,4,5,6) // Specify the type of values emitted (in this case, numbers)
-  .pipe(filter(value => value%2 !== 0))
-  .subscribe(result => console.log(result));
+    // of<number>(1, 2, 3,4,5,6) // Specify the type of values emitted (in this case, numbers)
+  // .pipe(filter(value => value%2 !== 0))
+  // .subscribe(result => console.log(result));
+
+  interval(1000).pipe(take(5)).subscribe(result => console.log(result))
   }
 }
