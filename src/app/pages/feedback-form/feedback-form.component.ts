c67e52f6ea87b6fbb4b5d8d671b2fc1fb56cc615
gokuljsparkout <gokul@sparkouttech.com>
2023-06-22 15:23:37 +0530

    Final

--- src/app/pages/feedback-form/feedback-form.component.ts
+++ src/app/pages/feedback-form/feedback-form.component.ts
@@ -15,33 +15,38 @@ export class FeedbackFormComponent {
   constructor() {}
 
   ngOnInit() {
+    this.initializeFbForm();
+    this.initializeGFbForm();
+    this.initializebRForm();
+    this.initializeFrForm();
+  }
+
+  onSubmit(form: FormGroup) {
+    if (form.valid) {
+      console.log( form.value);
+    }
+  }
+  initializeFbForm() {
     this.feedbackForm = new FormGroup({
-      formType: new FormControl('General Feedback', Validators.required),
-    });
-    this.generalFeedbackForm = new FormGroup({
-      feedback: new FormControl('',Validators.required),
-    });
-    this.bugReportForm = new FormGroup({
-      bugReportTitle: new FormControl('',Validators.required),
-      bugReportDetails: new FormControl('',Validators.required),
-    });
-    this.featureRequestForm = new FormGroup({
-      featureRequestTitle: new FormControl('',Validators.required),
-      featureRequestDescription: new FormControl('',Validators.required),
+      formCategory: new FormControl('General Feedback', Validators.required),
     });
   }
 
-  onSubmit(choice : number){
-    switch(choice){
-      case 1 : console.log(this.generalFeedbackForm.value);
-      break;
-      case 2 : console.log(this.bugReportForm.value);
-      break;
-      case 3 : console.log(this.featureRequestForm.value);
-      break;
-      default : console.log('end');
-      break;
-    }
-
+  initializeGFbForm() {
+    this.generalFeedbackForm = new FormGroup({
+      feedback: new FormControl('', Validators.required),
+    });
+  }
+  initializebRForm() {
+    this.bugReportForm = new FormGroup({
+      bugReportTitle: new FormControl('', Validators.required),
+      bugReportDetails: new FormControl('', Validators.required),
+    });
+  }
+  initializeFrForm() {
+    this.featureRequestForm = new FormGroup({
+      featureRequestTitle: new FormControl('', Validators.required),
+      featureRequestDescription: new FormControl('', Validators.required),
+    });
   }
 }
