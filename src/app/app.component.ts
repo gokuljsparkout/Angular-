19c1fb4edfa1f9df2eca03e32c9156da32cb6467
gokuljsparkout <gokul@sparkouttech.com>
2023-06-26 12:06:36 +0530

    Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { fromEvent, interval, of } from 'rxjs';
 import { debounceTime, mergeMap, mergeMapTo, take } from 'rxjs/operators';
+import { DefaultComponent } from './default/default.component';
+import { SignalsComponent } from './signals/signals.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [DefaultComponent, SignalsComponent],
 })
 export class AppComponent implements OnInit {
   constructor() {}
