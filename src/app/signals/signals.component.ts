d8d7d8a2fec37fcd64286c67b91f4fe81309929d
gokuljsparkout <gokul@sparkouttech.com>
2023-06-26 13:20:05 +0530

    Signal Equality

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,5 +1,13 @@
 import { NgFor } from '@angular/common';
-import { Component, OnInit, computed, effect, signal, untracked } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  computed,
+  effect,
+  signal,
+  untracked,
+} from '@angular/core';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-signals',
@@ -10,24 +18,23 @@ import { Component, OnInit, computed, effect, signal, untracked } from '@angular
 export class SignalsComponent implements OnInit {
   actions = signal<string[]>([]);
   counter = signal(0);
+  //if equal is true....effect wont trigger....effect wont trigger for the same value
   isEqual = computed(() => this.counter() === 5);
-constructor(){
-  effect(() => {
-    // console.log('Counter value', this.counter());
-    //if a signal state is used inside effect...
-    //the signal state becomes a dependency implicitly
+  constructor() {
+    effect(() => {
+      console.log('Counter value', this.counter());
+      //if a signal state is used inside effect...
+      //the signal state becomes a dependency implicitly
 
-    //if u want to use a signal inside effect but it not as a dependency...
-    //use untracked()
-    untracked(()=>console.log(this.actions())) 
-    // if (this.counter() === 5) {
-    //   console.log('Counter value is equal 5!');
-    // }
-  });
-}
-  ngOnInit() {
-    
+      //if u want to use a signal inside effect but it not as a dependency...
+      //use untracked()
+
+      if (this.counter() === 5) {
+        console.log('Counter value is equal 5!');
+      }
+    });
   }
+  ngOnInit() {}
 
   increment() {
     // this.counter.set(6);
@@ -37,7 +44,8 @@ constructor(){
   }
 
   decrement() {
-    this.counter.update((oldCounter) => oldCounter - 1);
+    // this.counter.update((oldCounter) => oldCounter - 1);
+    this.counter.set(5);
     this.actions.update((oldActions) => [...oldActions, 'DECREMENT']);
     // this.actions.push('DECREMENT');
   }
